7c103fbed0bda9c8da8efeeb999b5847a91a5c44
wouter0425 <56413873+wouter0425@users.noreply.github.com>
2022-06-03 14:27:02 +0200

    Update Slave.cpp

--- Slave.cpp
+++ Slave.cpp
@@ -48,11 +48,11 @@ void Slave::Get_Error_Registers(void)
 	int mask_first = 0xFF;	
 	int ret;
 
-	for (int i = 0; i < this->ports.size() && this->Get_Slave_Index() != 7; i++)
+	for (int i = 0; i < this->ports.size(); i++)
 	{		
 		//Get invalid frame counter (first byte) and rx error counter (second byte)
 		iter = std::to_string(this->Slave_Index);														
-		reg = std::to_string(ERR_OFFSET + (i*2));														
+		reg = std::to_string(ERR_OFFSET + (this.slaves[i].Get_Nr()*2));														
 		cmd = "L0=ecatregreadwrite(0,,1,,0,2);L0";													
 		cmd.insert(22, iter);
 		cmd.insert(26, reg);
@@ -62,7 +62,7 @@ void Slave::Get_Error_Registers(void)
 		this->ports[i].Set_RX_Error_Counter(ret & mask_first);										
 		
 		//Get the forwarded RX error counter
-		reg = std::to_string(ERR_OFFSET + i + 8);													
+		reg = std::to_string(ERR_OFFSET + this.slaves[i].Get_Nr() + 8);													
 		cmd = "L0=ecatregreadwrite(0,,1,,0,2);L0";													
 		cmd.insert(22, iter);
 		cmd.insert(26, reg);
@@ -70,7 +70,7 @@ void Slave::Get_Error_Registers(void)
 		this->ports[i].Set_Frame_Error_Counter(ret);									
 
 		//Get the lost link counter
-		reg = std::to_string(ERR_OFFSET + i + 16);													
+		reg = std::to_string(ERR_OFFSET + this.slaves[i].Get_Nr() + 16);													
 		cmd = "L0=ecatregreadwrite(0,,1,,0,2);L0";													
 		cmd.insert(22, iter);
 		cmd.insert(26, reg);
